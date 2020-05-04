ffb45df3c9fc4972b86894a1f6a55d8f7cbbd577
OlexandrKukhar <45728829+RedlineAlexander@users.noreply.github.com>
2020-05-04 09:36:24 +0300

    ConsumersControllers

--- BazarWithNormalLibraryMarketKukharITEA/Controllers/ConsumerController.cs
+++ BazarWithNormalLibraryMarketKukharITEA/Controllers/ConsumerController.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using BazarWithNormalLibraryMarketKukharITEA.Entities;
+using BazarWithNormalLibraryMarketKukharITEA.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BazarWithNormalLibraryMarketKukharITEA.Controllers
 {
-    public class ConsumerController
+
+    [Route("api/consumers")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
     {
+
+        readonly IService<Consumers> service;
+
+        public ConsumerController(IService<Consumers> service)
+        {
+            this.service = service;
+        }
+        [HttpGet]
+        public List<Consumers> Get()
+        {
+            return service.GetQuery().ToList();
+        }
+        [HttpGet("{id}")]
+        public Consumers Get(int id)
+        {
+            return service.FindById(id);
+        }
+        [HttpPost("save")]
+        public List<Consumers> Post([FromBody] Consumers value)
+        {
+            service.Create(value);
+            return service.GetAll().ToList();
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            service.Delete(id);
+        }
     }
 }
